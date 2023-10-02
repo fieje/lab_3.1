fd0b1b5352e38f79d9351806345e06641bd062af
Chukhrai <oleksandr.chukhrai@dspu.edu.ua>
2023-10-02 09:28:30 +0300

    initial commit

---
+++ FileName.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	double x; 
+	double y; 
+	double A;
+	double B; 
+	cout << "x = "; cin >> x;
+
+	A = 2*(0*(x-5));
+	if (x < -1)
+		B = (pow sin2(x))/(y+(0*cos(x))) ;
+	if (-1 <= x && x <= 1)
+		B = (pow cos2(x))*(1/|x+2|);
+	if (x > 1)
+		B = log10*|x+2|;
+	y = A - B;
+	cout << endl;
+	cout << "1) y = " << y << endl;	cin.get();
+	return 0;
+}
